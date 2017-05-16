0f8b0e40b2ccfa0ae537830baa2c820bcb117b36
Rainyuting <siyangyang@Siyangs-MacBook-Air.local>
2017-05-16 15:28:45 -0700

    implemented web view. Everything works so far

--- app/src/main/java/edu/ucla/bruinfo/MainActivity.java
+++ app/src/main/java/edu/ucla/bruinfo/MainActivity.java
@@ -1,13 +1,51 @@
 package edu.ucla.bruinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import android.webkit.WebResourceRequest;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    WebView mWebview;
+    WebSettings mWebSettings;
+    TextView beginLoading,endLoading,loading,mtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        mWebview = (WebView) findViewById(R.id.webView1);
+        beginLoading = (TextView) findViewById(R.id.text_beginLoading);
+        endLoading = (TextView) findViewById(R.id.text_endLoading);
+        loading = (TextView) findViewById(R.id.text_Loading);
+        mtitle = (TextView) findViewById(R.id.title);
+
+        mWebSettings = mWebview.getSettings();
+
+        mWebview.loadUrl("http://www.yahoo.com/");
+
+
+// load inside of webview
+        //mWebview.setWebViewClient(new WebViewClient() {
+           // @Override
+            //public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+          //      view.loadUrl("http://www.google.com/");
+            //    return true;
+       //     }
+       // });
+
+
+
+}
 }
