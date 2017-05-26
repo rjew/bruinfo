a2f7c29fbef46f0a60a2c90a8ae3e04a9b9139f8
Russell Jew <russelljew104@gmail.com>
2017-05-25 17:07:59 -0700

    Removed unused import statement

--- app/src/main/java/edu/ucla/bruinfo/MainActivity.java
+++ app/src/main/java/edu/ucla/bruinfo/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.Toast;
